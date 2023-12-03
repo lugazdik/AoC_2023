3210bc1c11f1100d16f3d6fd2637b4fcdc447535
Lukas Gazdik <lukas@epigenelabs.com>
2023-12-03 12:50:03 +0100

    :art: Refactored solution day 3

--- day03/day03.py
+++ day03/day03.py
@@ -1,6 +1,6 @@
 def read_input(
     input_path: str,
-) -> tuple[list[tuple[int, int, int]], list[tuple[int, int]]]:
+) -> tuple[list[tuple[int, int, int]], list[tuple[int, int, str]]]:
     parts = []
     symbols = []
     with open(input_path, "r") as f:
@@ -25,7 +25,7 @@ def read_input(
                         number = ""
                         number_start_index = -1
                     if symbol != ".":
-                        symbols.append((row, column))
+                        symbols.append((row, column, symbol))
             if number:
                 row_numbers.append(
                     (
@@ -38,83 +38,52 @@ def read_input(
         return parts, symbols
 
 
-def read_input2(
-    input_path: str,
-) -> tuple[list[tuple[int, int, int]], list[tuple[int, int]]]:
-    parts = []
-    symbols = []
-    with open(input_path, "r") as f:
-        for row, line in enumerate(f.readlines()):
-            row_numbers = []
-            number = ""
-            number_start_index = -1
-            for column, symbol in enumerate(line.strip()):
-                if symbol.isnumeric():
-                    number += symbol
-                    if number_start_index == -1:
-                        number_start_index = column
-                else:
-                    if number:
-                        row_numbers.append(
-                            (
-                                int(number),
-                                number_start_index,
-                                number_start_index + len(number) - 1,
-                            )
-                        )
-                        number = ""
-                        number_start_index = -1
-                    if symbol == "*":
-                        symbols.append((row, column))
-            if number:
-                row_numbers.append(
-                    (
-                        int(number),
-                        number_start_index,
-                        number_start_index + len(number) - 1,
-                    )
-                )
-            parts.append(row_numbers)
-        return parts, symbols
+def is_adjacent(row, start, end, symbol_row, symbol_column):
+    if abs(row - symbol_row) > 1 or (
+        abs(start - symbol_column) > 1 and abs(end - symbol_column) > 1
+    ):
+        return False
+    return True
 
 
 def part1(
     parts: list[tuple[int, int, int]],
-    symbols: list[tuple[int, int]],
+    symbols: list[tuple[int, int, str]],
 ) -> int:
     result = []
-    rows = []
     for row, row_parts in enumerate(parts):
-        row_counting = []
         for row_part in row_parts:
             for symbol in symbols:
-                if abs(row - symbol[0]) > 1 or (
-                    abs(row_part[1] - symbol[1]) > 1
-                    and abs(row_part[2] - symbol[1]) > 1
+                if is_adjacent(
+                    row,
+                    row_part[1],
+                    row_part[2],
+                    symbol[0],
+                    symbol[1],
                 ):
-                    continue
-                else:
-                    row_counting.append(row_part[0])
                     result.append(row_part[0])
-        rows.append(row_counting)
+                    break
     return sum(result)
 
 
 def part2(
     parts: list[tuple[int, int, int]],
-    symbols: list[tuple[int, int]],
+    symbols: list[tuple[int, int, str]],
 ) -> int:
     result = {}
     for symbol in symbols:
+        if symbol[2] != "*":
+            continue
         result[symbol] = []
         for row, row_parts in enumerate(parts):
             for row_part in row_parts:
-                if abs(row - symbol[0]) > 1 or (
-                    abs(row_part[1] - symbol[1]) > 1
-                    and abs(row_part[2] - symbol[1]) > 1
+                if is_adjacent(
+                    row,
+                    row_part[1],
+                    row_part[2],
+                    symbol[0],
+                    symbol[1],
                 ):
-                    continue
-                else:
                     result[symbol].append(row_part[0])
     gear_ratio = 0
     for values in result.values():
@@ -125,5 +94,4 @@ def part2(
 
 parsed_parts, parsed_symbols = read_input("day03.txt")
 print(part1(parsed_parts, parsed_symbols))
-parsed_parts2, parsed_symbols2 = read_input2("day03.txt")
-print(part2(parsed_parts2, parsed_symbols2))
+print(part2(parsed_parts, parsed_symbols))
