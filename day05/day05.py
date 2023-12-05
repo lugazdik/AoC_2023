9891180da8c1adf604f28fd0289a42a3c0b01fa5
Lukas Gazdik <lukas@epigenelabs.com>
2023-12-05 17:29:13 +0100

    :art: Day 05 refactor

--- day05/day05.py
+++ day05/day05.py
@@ -35,81 +35,51 @@ def part1(
     return min(seeds)
 
 
-def does_fully_contain(a, b, x, y):
-    return (a <= x <= b and a <= y <= b) or (x <= a <= y and x <= b <= y)
+def find_interval_overlap(a: int, b: int, x: int, y: int):
+    if a < x and y < b:
+        # a-x-y-b
+        return [(x, y)], [(a, x - 1), (y + 1, b)]
+    elif a < x <= b:
+        # a-x-b-y
+        return [(x, b)], [(a, x - 1)]
+    elif a <= y < b:
+        # x-a-y-b
+        return [(a, y)], [(y + 1, b)]
+    elif x <= a and b <= y:
+        # x-a-b-y
+        return [(a, b)], []
+    else:
+        # a-b-x-y or x-y-a-b
+        return [], [(a, b)]
 
 
-def does_overlap(a, b, x, y):
-    return a <= x <= b or a <= y <= b or x <= a <= y or x <= b <= y
-
-
-def find_seed_interval_destination(
-    seed_interval: tuple[int, int], converters: list[tuple[int, int, int]]
-) -> list[tuple[int, int]]:
-    result_intervals = []
-    for interval in converters:
-        if interval[1] <= seed_interval[0]:
-            if seed_interval[0] + seed_interval[1] <= interval[1] + interval[2]:
-                return result_intervals + [
-                    (interval[0] + seed_interval[0] - interval[1], seed_interval[1])
-                ]
-            if seed_interval[0] < interval[1] + interval[2]:
-                covered_start = interval[0] + seed_interval[0] - interval[1]
-                covered_end = interval[2] - (seed_interval[0] - interval[1])
-                # print(
-                #     f"partial {seed_interval}, {interval} = {(covered_start, covered_end)}, continue with {(covered_start + covered_end, seed_interval[1] - covered_end)}"
-                # )
-                result_intervals.append((covered_start, covered_end))
-                seed_interval = (
-                    covered_start + covered_end,
-                    seed_interval[1] - covered_end,
-                )
-            else:
-                pass
-                # print(
-                #     f"no overlap converter bigger {seed_interval}, {interval} = {seed_interval}"
-                # )
-        else:
-            if seed_interval[0] + seed_interval[1] > interval[1]:
-                not_covered_start = seed_interval[0]
-                not_covered_end = interval[1] - seed_interval[0]
-                covered_start = interval[0]
-                covered_end = seed_interval[1] - not_covered_end
-                # print(
-                #     f"SECOND partial covered  {seed_interval}, {interval} = {(covered_start, covered_end)}, continue with {(not_covered_start, not_covered_end)}"
-                # )
-                result_intervals.append((covered_start, covered_end))
-                seed_interval = (not_covered_start, not_covered_end)
-            elif (
-                seed_interval[0] <= interval[1]
-                and seed_interval[0] + seed_interval[1] > interval[1] + interval[2]
-            ):
-                print(f"interval fully covered by seed {seed_interval}, {interval}")
-            else:
-                pass
-                # print(
-                #     f"NOT IMPLEMENTED seed start smaller than converter {seed_interval}, {interval}, continue"
-                # )
-    if seed_interval:
-        result_intervals.append(seed_interval)
-    return result_intervals
-
-
-def part2(
-    seeds: list[int],
-    converters: list[list[tuple[int, int, int]]],
-) -> int:
-    seed_pairs = []
-    for index in range(0, len(seeds), 2):
-        seed_pairs.append((seeds[index], seeds[index + 1]))
+def part2(seeds: list[int], converters: list[list[tuple[int, int, int]]]):
+    seed_pairs = [
+        (seeds[i], seeds[i] + seeds[i + 1] - 1) for i in range(0, len(seeds), 2)
+    ]
+    converters = [
+        [[dest - start, start, start + size - 1] for dest, start, size in converter]
+        for converter in converters
+    ]
     for converter in converters:
-        new_seed_pairs = []
+        new_ranges = []
         for seed_pair in seed_pairs:
-            new_seed_pairs += find_seed_interval_destination(seed_pair, converter)
-        seed_pairs = new_seed_pairs
-    return min(seed_pairs, key=lambda x: x[0])[0]
+            unprocessed = [seed_pair]
+            for offset, converter_start, converter_end in converter:
+                new_unprocessed = []
+                for seed_start, seed_end in unprocessed:
+                    found_interval, extra = find_interval_overlap(
+                        seed_start, seed_end, converter_start, converter_end
+                    )
+                    new_unprocessed += extra
+                    new_ranges += [(a + offset, b + offset) for a, b in found_interval]
+                unprocessed = new_unprocessed
+            new_ranges += unprocessed
+        seed_pairs = new_ranges
+
+    return min(start for start, _ in seed_pairs)
 
 
 input_seeds, input_converters = read_input("day05.txt")
-# print(part1(input_seeds, input_converters))
-print(f"result {part2(input_seeds, input_converters)}")
+print(part1(input_seeds, input_converters))
+print(part2(input_seeds, input_converters))
